0ebe50b6464056b546448951006440be4f72ee04
wolfgang <wolfgang.zhu@zenlayer.com>
2023-08-14 15:14:40 +0800

    release 0.1.5 / Add DescribeLogs

---
+++ zenlayercloud/zls20230804/client.go
@@ -0,0 +1,59 @@
+package zls
+
+import (
+	"github.com/zenlayer/zenlayercloud-sdk-go/zenlayercloud/common"
+)
+
+const (
+	APIVersion = "2023-08-04"
+	SERVICE    = "zls"
+)
+
+type Client struct {
+	common.Client
+}
+
+func NewClientWithSecretKey(secretKeyId, secretKeyPassword string) (client *Client, err error) {
+	return NewClient(common.NewConfig(), secretKeyId, secretKeyPassword)
+}
+
+func NewClient(config *common.Config, secretKeyId, secretKeyPassword string) (client *Client, err error) {
+	client = &Client{}
+
+	err = client.InitWithCredential(common.NewCredential(secretKeyId, secretKeyPassword))
+	if err != nil {
+		return nil, err
+	}
+	err = client.WithConfig(config)
+
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func NewDescribeLogsRequest() (request *DescribeLogsRequest) {
+	request = &DescribeLogsRequest{
+		BaseRequest: &common.BaseRequest{},
+	}
+	request.Init().InitWithApiInfo(SERVICE, APIVersion, "DescribeLogs")
+
+	return
+}
+
+// DescribeLogs
+// 本接口（DescribeLogs）用于查询审计日志列表。
+//
+// Possible error codes to return:
+func (c *Client) DescribeLogs(request *DescribeLogsRequest) (response *DescribeLogsResponse, err error) {
+	response = NewDescribeLogsResponse()
+	err = c.ApiCall(request, response)
+	return
+}
+
+func NewDescribeLogsResponse() (response *DescribeLogsResponse) {
+	response = &DescribeLogsResponse{
+		BaseResponse: &common.BaseResponse{},
+	}
+	return
+}
---
+++ zenlayercloud/zls20230804/models.go
@@ -0,0 +1,61 @@
+package zls
+
+import "github.com/zenlayer/zenlayercloud-sdk-go/zenlayercloud/common"
+
+type DescribeLogsRequest struct {
+	*common.BaseRequest
+
+	StartTime string `json:"startTime,omitempty"`
+
+	EndTime string `json:"endTime,omitempty"`
+
+	ResUid string `json:"resUid,omitempty"`
+
+	ResEvent string `json:"resEvent,omitempty"`
+
+	ClientIP string `json:"clientIP,omitempty"`
+
+	Size int `json:"size,omitempty"`
+
+	Cursor []*interface{} `json:"cursor,omitempty"`
+}
+
+type DescribeLogsResponse struct {
+	*common.BaseResponse
+
+	// The unique request ID, which is returned for each request. RequestId is required for locating a problem.
+	RequestId string `json:"requestId,omitempty"`
+
+	Response *DescribeLogsResponseParam `json:"response"`
+}
+
+type DescribeLogsResponseParam struct {
+	// The unique request ID, which is returned for each request. RequestId is required for locating a problem.
+	RequestId string `json:"requestId,omitempty"`
+
+	Cursor []*interface{} `json:"cursor,omitempty"`
+
+	DataSet []*LogInfo `json:"dataSet,omitempty"`
+}
+
+type LogInfo struct {
+	ResUid string `json:"resUid,omitempty"`
+
+	ResType string `json:"resType,omitempty"`
+
+	ResEvent string `json:"resEvent,omitempty"`
+
+	OpsTime string `json:"opsTime,omitempty"`
+
+	EventSource string `json:"eventSource,omitempty"`
+
+	ApiVersion string `json:"apiVersion,omitempty"`
+
+	OpsUser string `json:"opsUser,omitempty"`
+
+	ClientIP string `json:"clientIP,omitempty"`
+
+	Request string `json:"request,omitempty"`
+
+	Response string `json:"response,omitempty"`
+}
