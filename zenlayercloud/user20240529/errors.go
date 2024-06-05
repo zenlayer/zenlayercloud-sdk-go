6c7cc783a9a5e8072b48116fe9aa2decbce8f6a6
wolfgang <wolfgang.zhu@zenlayer.com>
2024-06-05 14:27:39 +0800

    release 0.1.15: Add userAPI about ResourceGroup

---
+++ zenlayercloud/user20240529/errors.go
@@ -0,0 +1,12 @@
+/*
+ * Zenlayer.com Inc.
+ * Copyright (c) 2014-2023 All Rights Reserved.
+ */
+package user
+
+const (
+	// This product has a unique error code
+
+	// parameter illegal time
+	INVALID_PARAMETER__ILLEGAL_TIME = "Invalid.Paramter.Illegal.Time"
+)
--- zenlayercloud/zec20240401/models.go
+++ zenlayercloud/zec20240401/models.go
@@ -44,6 +44,14 @@ type DescribeInstanceMonitorDataRequest struct {
 	//INTERNET_EGRESS_BITS: 公网出向带宽，单位bps
 	MetricType string `json:"metricType,omitempty"`
 
+	//公网IP地址。
+	//当存在多个IP时，需要指定对应网卡上的公网地址。该字段仅对下列指标有效。
+	//INTERNET_INGRESS_BITS
+	//INTERNET_EGRESS_BITS
+	//INTERNET_INGRESS_PACKETS
+	//INTERNET_EGRESS_PACKETS
+	IpAddress string `json:"ipAddress,omitempty"`
+
 	//查询开始时间。
 	//按照ISO8601标准表示，并且使用UTC时间。格式为：YYYY-MM-DDThh:mm:ssZ。
 	StartTime string `json:"startTime,omitempty"`
