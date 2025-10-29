be3df9fcb97009f1c9248488594dd8f930378dd4
wolfgang.zhu <wolfgang.zhu@zenlayer.com>
2025-10-29 09:25:45 +0800

    release 0.2.4
    Add pointer to value function

--- zenlayercloud/common/types.go
+++ zenlayercloud/common/types.go
@@ -27,3 +27,52 @@ func Bool(v bool) *bool {
 func String(v string) *string {
 	return &v
 }
+
+func ToInteger(v *int) int {
+	if v == nil {
+		return 0
+	}
+	return *v
+}
+
+func ToInt64(v *int64) int64 {
+	if v == nil {
+		return 0
+	}
+	return *v
+}
+
+func ToUint(v *uint) uint {
+	if v == nil {
+		return 0
+	}
+	return *v
+}
+
+func ToUint64(v *uint64) uint64 {
+	if v == nil {
+		return 0
+	}
+	return *v
+}
+
+func ToFloat64(v *float64) float64 {
+	if v == nil {
+		return 0
+	}
+	return *v
+}
+
+func ToBool(v *bool) bool {
+	if v == nil {
+		return false
+	}
+	return *v
+}
+
+func ToString(v *string) string {
+	if v == nil {
+		return ""
+	}
+	return *v
+}
